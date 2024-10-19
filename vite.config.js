7e907dd7685bf8676ac36b25feab32fffeb0f8d4
Yuvraj Yadav <yuvraj7000raju@gmail.com>
2024-10-19 15:46:18 +0530

    Refactor proxy configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://oyster-app-jndzv.ondigitalocean.app',
+        changeOrigin: true,
         secure: false,
       },
     },
